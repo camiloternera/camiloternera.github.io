6e9e7fb248270c7d9a768d0736a383d89db7c96d
Camilo Ternera <camiloterneraduque.gmail.com>
2021-04-15 01:08:47 -0500

    Inicio complete, perfil complete

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// console.log("funcionando");
+(function () {
+  "use strict";
+  addEventListener("DOMContentLoaded", () => {
+    /* Utilizando libreria TYPED.JS
+      https://github.com/mattboldt/typed.js/
+    */
+    function animationText() {
+      if (document.getElementById("frases")) {
+        const typed = new Typed(".typed", {
+          // strings: [
+          //   "Capacitate con nosotros",
+          //   "Da el primer paso para cumplir tus sueños",
+          // ],
+          stringsElement: "#frases", // ID del elemento que contiene cadenas de texto a mostrar.
+          typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+          startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+          backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+          smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+          shuffle: false, // Alterar el orden en el que escribe las palabras.
+          backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+          loop: true, // Repetir el array de strings
+          loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+          showCursor: true, // Mostrar cursor palpitanto
+          cursorChar: "|", // Caracter para el cursor
+          contentType: "html", // 'html' o 'null' para texto sin formato
+        });
+      }
+    }
+    animationText();
+  });
+})();
